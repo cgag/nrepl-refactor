9a57f23a5ca0343f27ca4de6b1e0b555d533599d
Curtis Gagliardi <cgag@riseup.net>
2014-02-07 01:24:20 -0800

    Add some more refactors. Cycle privacy/collection/keyword->string.

--- src/io/curtis/nrepl_refactor.clj
+++ src/io/curtis/nrepl_refactor.clj
@@ -1,6 +1,5 @@
 (ns io.curtis.nrepl-refactor
-  (:require [clojure.pprint :refer [pprint]]
-            [clojure.walk :as walk]
+  (:require [clojure.walk :as walk]
             [clojure.tools.nrepl.transport :as t]
             [clojure.tools.nrepl.misc :refer [response-for]]
             [clojure.tools.nrepl.middleware :refer [set-descriptor!]]
@@ -9,7 +8,6 @@
                                                 default-handler]]
             [clojure.tools.nrepl :as repl]))
 
-;; TODO: bug? Why is  (= (reverse forms) (reverse (reverse forms))) false?
 (defn thread-last [form]
   (letfn [(thread-last-forms* [form] 
             (if-not (list? form) 
@@ -42,12 +40,47 @@
 (defn unthread-first [form]
   (macroexpand-only #{'-> `->} form))
 
+(defn cycle-collection-type [coll]
+  (let [class->pair {clojure.lang.PersistentList     [1 "{" "}"]
+                     clojure.lang.PersistentArrayMap [1 "[" "]"]
+                     clojure.lang.PersistentVector   [1 "#{" "}"]
+                     clojure.lang.PersistentHashSet  [2 "(" ")"]}
+        [to-drop open close] (class->pair (class coll))
+        coll-str (pr-str coll)
+        s (str open (subs coll-str 
+                          to-drop 
+                          (- (count coll-str) 1)) 
+               close)]
+    s))
 
+;; TODO: is evaling this a bad idea?
+(defn cycle-privacy [fn-form]
+  (let [f (first fn-form)
+        new-fn-form (condp = f
+                      'defn (cons 'defn- (drop 1 fn-form))
+                      'defn- (cons 'defn (drop 1 fn-form))
+                      'def (if (:private (meta (eval fn-form)))
+                             (pr-str fn-form)
+                             (apply str 
+                                    (concat "(def ^:private"
+                                            (drop (count "(def") (pr-str fn-form)))))
+                      )]
+    new-fn-form))
+
+(defn cycle-str-keyword [s-or-k]
+  (condp = (class s-or-k)
+    clojure.lang.Keyword (subs (str s-or-k) 1)
+    java.lang.String (keyword s-or-k)))
+
+;; TODO: maybe op should just be refactor and another key should specify which?
 (def refactor-fns {"refactor.identity"       identity
                    "refactor.thread-last"    thread-last
                    "refactor.thread-first"   thread-first
                    "refactor.unthread-last"  unthread-last
-                   "refactor.unthread-first" unthread-first})
+                   "refactor.unthread-first" unthread-first
+                   "refactor.cycle-str-keyword" cycle-str-keyword
+                   "refactor.cycle-privacy" cycle-privacy
+                   "refactor.cycle-collection-type" cycle-collection-type})
 
 (defn refactor [h]
   (fn [{:keys [op code transport] :as msg}]
@@ -73,10 +106,10 @@
   (comment (stop-server server))
   
   ;; TODO: real tests, generative tests that these are inverses of each other
-  (unthread-last '(->> [1 2 3 4 5] (filter even?) (map square) (map f)))
-  (unthread-first '(-> [1 2] b c d))
-  (thread-first '(d (b (c a))))
-  (thread-last '(map f (map square (filter even? [1 2 3 4 5]))))
+  ;(unthread-last '(->> [1 2 3 4 5] (filter even?) (map square) (map f)))
+  ;(unthread-first '(-> [1 2] b c d))
+  ;(thread-first '(d (b (c a))))
+  ;(thread-last '(map f (map square (filter even? [1 2 3 4 5]))))
 
   (with-open [conn (repl/connect :port port)]
     (-> (repl/client conn 1000)                                                 
